d26e27412bf70100b4ac9c50a27487c8b8374ee4
Jere Laitila <jere.laitila@metropolia.fi>
2021-03-11 20:32:04 +0200

    osa3 p1

--- osa3/index.js
+++ osa3/index.js
@@ -96,7 +96,7 @@ app.post('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
