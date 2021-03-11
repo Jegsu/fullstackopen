3e6af80efc377fd44a5f76913c72f2367f0d59ab
Jere Laitila <jere.laitila@metropolia.fi>
2021-03-11 20:09:48 +0200

    osa3 p1

--- osa3/index.js
+++ osa3/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+// "tiny" with :body
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+})
 
 let persons = [
     {
@@ -85,7 +96,7 @@ app.post('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
